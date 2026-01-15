696842406edf5f1b601e4e3ecc4b3c642c6f38cd
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-01-15 19:50:43 +0100

    Terminato Selfwork Funzioni 4

--- main.js
+++ main.js
@@ -7,16 +7,30 @@
 // Output: true
 
 
-let check = false;
 let str = 'i topi non avevano nipoti';
-str = str.replace(/\W/g, "");
-let reversed = str.split('').reverse().join('');
 
-if(str == reversed){
-    check = true;
-}
-else{
-    check = false;
+function clearString(string){
+    return string.replace(/\W/g, "");
 }
 
+function reverseString(string){
+    return string.split('').reverse().join('');
+}
+
+function palindromeString(str1, str2) {
+    if(str1 == str2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+let cleared = clearString(str);
+console.log(cleared);
+
+let reversed = reverseString(cleared);
+console.log(reversed);
+
+let check = palindromeString(cleared, reversed);
 console.log(check);
